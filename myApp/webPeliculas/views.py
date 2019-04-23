32bee53392d6fc061dc7631b197b272370433d99
alebarrod <alebarrod@alum.us.es>
2019-04-23 13:40:34 +0200

    route '/actores' return Actor order by its name 'nombre'

--- myApp/webPeliculas/views.py
+++ myApp/webPeliculas/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse, HttpResponseNotFound,JsonResponse
 from webPeliculas.models import Actor
 
 def actoresPorNombre(request):
-    a = list(Actor.objects.all().values())
+    a = list(Actor.objects.order_by('nombre').values('nombre','apellidos','biografia'))
 
     return JsonResponse(a, safe=False)
